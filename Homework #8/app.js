a93dc65b44fc6524826dc91f5448fd64acc28134
ViktorS001 <96490594+ViktorS001@users.noreply.github.com>
2021-12-27 02:03:12 +0100

    Update app.js

--- Homework #8/app.js
+++ Homework #8/app.js
@@ -1,14 +1,38 @@
-var phone = 119.95; //price of units in dollar
-var units = 30; //how many units are needed
-var tax = phone * units * 0.05; //5% tax calculated
+var phonePrice = 119.95; //price of units in dollar
+var initialUnits = 30; //how many units are needed
+var tax = phonePrice * initialUnits * 0.05; //5% tax calculated
 
-alert("the price for 30 phones including 5% tax is " + (phone * units + tax) + "$");//price of 30 units with tax
-console.log("the price for 30 phones including 5% tax is " + (phone * units + tax) + "$");
+alert(
+  "the price for 30 phones including 5% tax is " +
+    (phonePrice * initialUnits + tax) +
+    "$"
+); //price of 30 units with tax
+console.log(
+  "the price for 30 phones including 5% tax is " +
+    (phonePrice * initialUnits + tax) +
+    "$"
+);
 
-var unit = prompt("please enter how many more units you will like to buy");//if you would like to buy more than 30 units
-alert("the price for " +unit+ " phones including 5% tax is: "  + (phone * unit + tax) + "$");
-console.log("the price for " +unit+ " phones including 5% tax is: "  + (phone * unit + tax) + "$");
-
-var total = (phone * units + tax) + (phone * unit + tax);//total price
-alert("total: "  + total + "$");
-console.log("total: "  + total + "$");
+var extraUnits = prompt(
+  "please enter how many more units you will like to buy"
+); //if you would like to buy more than 30 units
+var taxExtraunits = phonePrice * extraUnits * 0.05;
+alert(
+  "the price for " +
+    extraUnits +
+    " phones including 5% tax is: " +
+    (phonePrice * extraUnits + taxExtraunits) +
+    "$"
+);
+console.log(
+  "the price for " +
+    extraUnits +
+    " phones including 5% tax is: " +
+    (phonePrice * extraUnits + taxExtraunits) +
+    "$"
+);
+//5% tax calculated
+var total =
+  phonePrice * extraUnits + taxExtraunits + (phonePrice * initialUnits + tax); //total price
+alert("total: " + total + "$");
+console.log("total: " + total + "$");
